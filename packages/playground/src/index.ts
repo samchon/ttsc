d60e9ebb9dcef2ddb8f955421aaaf1071a595839
samchon <samchon.github@gmail.com>
2026-05-25 12:30:02 +0900

    feat(playground): add npm dependency loader

---
+++ packages/playground/src/index.ts
@@ -0,0 +1,11 @@
+export {
+  BUILT_IN_PLAYGROUND_PACKAGES,
+  collectExternalPackageNames,
+  installPlaygroundDependencies,
+  packageNameFromSpecifier,
+  type IPlaygroundDependencyInstallOptions,
+  type IPlaygroundDependencyInstallResult,
+  type IPlaygroundDependencyPackage,
+  type IPlaygroundDependencyProgress,
+  type IPlaygroundDependencyProgressPhase,
+} from "./npm-dependencies";
